5dbad77fe0d565a712fd31c3631dcfeed1b7a747
CamiloJSolis <cjsolis@correo.url.edu.gt>
2026-01-30 20:13:58 -0600

    Update timestamp format in saludo.js

--- api/saludo.js
+++ api/saludo.js
@@ -2,6 +2,6 @@ export default function handler(req,res) {
   res.status(200).json({
     mensaje:"こんにちはISW2！",
     curso:"ソフトウェアエンジニアリング二",
-    timestamp:new Date().toISOString()
+    timestamp:"..."
   });
 }
