a83c13300a37282c40467b4ba9ade3742b77a3fa
CamiloJSolis <cjsolis@correo.url.edu.gt>
2026-02-03 22:03:26 -0600

    Implement error handling and fix typo in procesar.js
    Added error handling for 'error' input and fixed a typo in the default name.

--- api/procesar.js
+++ api/procesar.js
@@ -1,7 +1,15 @@
-export default function handler(req, res) {
-  const nombre = req.query.nombre || "anṕnimo";
-  
+ export default function handler(req, res) {
+  const nombre = req.query.nombre || "anónimo";
+
+   if (nombre.toLowerCase() === "error") {
+     return req.status(500).json({
+       error: "ATENCIÓN!!! Te volaste el server",
+       timestamp: new Date().toISOString(),
+     });
+   }
+   
   res.status(200).json({
     resultado:`Nombre procesado: ${nombre.toUpperCase()}`
+    timestamp: new Date().toISOString(),
   });
 }
